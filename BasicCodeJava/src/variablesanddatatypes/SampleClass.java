0217c1ccbe1fe6befd0bc389b1f081389d6f18e9
RAJ <RAJ@RAJKUMAR>
2019-12-07 15:42:59 +0530

    first file updated

--- BasicCodeJava/src/variablesanddatatypes/SampleClass.java
+++ BasicCodeJava/src/variablesanddatatypes/SampleClass.java
@@ -4,7 +4,13 @@ public class SampleClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Java");
 
 	}
 
 }
+// create\delete\modify files
+//currently on unstaged condition
+//convert it to stagged
+//then comment whether its been created or modified etc
+//finally commit and push the file
