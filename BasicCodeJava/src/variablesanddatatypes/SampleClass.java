97984f54a6feaf8f420ed254d00c955fdf01225b
RAJ <RAJ@RAJKUMAR>
2019-12-08 13:48:04 +0530

    Datatypes and variables creation

--- BasicCodeJava/src/variablesanddatatypes/SampleClass.java
+++ BasicCodeJava/src/variablesanddatatypes/SampleClass.java
@@ -3,9 +3,24 @@ package variablesanddatatypes;
 public class SampleClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Java");
-
+		String name= "Nisha"; //reserved data types(class,arrays,and interfaces)
+		int age=24;      //primitive data types
+		char gender='F'; //primitive data types
+		double height= 5.7;
+		boolean married= false;
+		
+		//other premitive datatypes
+		byte a=10;
+		short b=200;
+		int c=300;
+		long d=123456; //HW differcene between these four datatypes
+						//Also how many total reserved keywords are there in JAVA
+		
+		//Decimal
+		float f=20.67f;
+		double e=765.26900;  //HW difference between these two dataypes
+		
+		
 	}
 
 }
